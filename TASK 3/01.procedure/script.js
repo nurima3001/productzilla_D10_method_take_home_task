3c2323525efc231a098f2c7d0298a97e7bab3120
nurima3001 <126258902+nurima3001@users.noreply.github.com>
2023-03-14 18:46:51 +0700

    Add files via upload

---
+++ TASK 3/01.procedure/script.js
@@ -0,0 +1,81 @@
+function set(NamaDepan) {
+    nama_depan = argNamaDepan
+}
+const nama= set(`adi`)
+console.log(nama)
+
+function set(NamaBelakang) {
+    nama_belakang = argNamaBelakang
+}
+
+function setUmur(argUmur) {
+    umur = argUmur
+}
+
+function setAlamat(argAlamat) {
+    alamat = argAlamat
+}
+
+function setEmail(argEmail) {
+    email = argEmail
+}
+
+function set( NomorHP) {
+    NomorHP = argNomorHP
+}
+
+function set(JenisKelamin) {
+    jenis_Kelamin = argJenisKelamin
+}
+
+function set(Hobby) {
+    Hobby = argHobby
+}
+
+function set(Pendidikan) {
+    Pendidikan = argPendidikan
+}
+
+function set(Pekerjaan) {
+    Pekerjaan = argPekerjaan
+}
+
+function set(Gaji) {
+    Gaji = argGaji
+}
+
+function set(PekerjaanAyah) {
+    pekerjaan_ayah = argPekerjaanAyah
+}
+
+function set(PekerjaanIbu) {
+    pekerjaan_ayah = argPekerjaanIbu
+}
+
+function set(TanggalLahir) {
+    tanggal_Lahir = argTanggalLahir
+}
+
+function set(Negara) {
+    Negara = argNegara
+}
+
+function set(Kota) {
+    Kota = argKota
+}
+
+function set(GolonganDarah) {
+    GolonganDarah = argGolonganDarah
+}
+
+function set(NomorKTP) {
+    NomorKTP = argNomorKTP
+}
+
+function set(NamaSaudara) {
+    NamaSaudara = argNamaSaudara
+}
+
+function set(TeleponRummah) {
+    telepon_rumah = argTeleponRumah
+}
---
+++ TASK 3/02.Function/script.js
@@ -0,0 +1,98 @@
+function getNamaDepan() {
+    return NamaDepan;
+}
+console.log(getNamaDepan());
+
+function get(NamaBelakang) {
+    return NamaBelakang;
+}
+console.log(getNamaBelakang());
+
+function getUmur(argUmur) {
+    return argUmur;
+}
+console.log(Umur());
+    
+function setAlamat(argAlamat) {
+    return Alamat;
+}console.log(getAlamat());
+
+function setEmail(argEmail) {
+    return Email;
+}
+console.log(getEmail());
+
+function set( NomorHP) {
+    return NomorHP;
+}
+console.log(getNomorHP());
+
+function set(JenisKelamin) {
+    return JenisKelamin;
+}
+console.log(getJenisKelamin());
+
+function set(Hobby) {
+    return Hobby;
+}
+console.log(getHobby());
+
+function set(Pendidikan) {
+    return Pendidikan;
+}
+console.log(getpendidikan());
+
+function set(Pekerjaan) {
+    return Pekerjaan;
+}
+console.log(getpekerjaan());
+
+function set(Gaji) {
+    return Gaji;
+}
+console.log(getGaji());
+
+function set(PekerjaanAyah) {
+    return pekerjaan_ayah;
+}
+console.log(getpekerjaan());
+
+function set(PekerjaanIbu) {
+    return pekerjaan_ayah;
+}
+console.log(getpekerjaan());
+
+function set(TanggalLahir) {
+    return TanggalLahir;
+}
+console.log(getTanggalLahir());
+
+function set(Negara) {
+    return Negara;
+}
+console.log(getNegara());
+
+function set(Kota) {
+    return Kota;
+}
+console.log(getKota());
+
+function set(GolonganDarah) {
+    return GolonganDarah;
+}
+console.log(getGolonganDarah());
+
+function set(NomorKTP) {
+    return NomorKTP;
+}
+console.log(getNomorKTP());
+
+function set(NamaSaudara) {
+    return NamaSaudara;
+}
+console.log(getNamaSaudara());
+
+function set(TeleponRummah) {
+    return TeleponRummah;
+}
+console.log(getTeleponRumah());
